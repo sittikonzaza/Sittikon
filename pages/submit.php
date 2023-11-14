22eeaf81a5d336a398559953d6b426229f6d0152
sittikonzaza <sittikon2345@gmail.com>
2023-11-14 11:10:33 +0700

    submit.php

---
+++ pages/submit.php
@@ -0,0 +1,22 @@
+<?php
+$email = $_POST['email'];
+$email = $_POST['password'];
+$email = $_POST['City'];
+$email = $_POST['web'];
+$email = $_POST['role'];
+$email = $_POST['sign'];
+
+
+echo ' email: '.$email.'<br>';
+echo ' password: '.$password.'<br>';
+echo ' city: '.$City.'<br>';
+echo ' web: '.$web.'<br>';
+echo ' role: '.$role.'<br>';
+echo ' sign: '.$sign.'<br>';
+echo ' sign1: '.$sign1.'<br>';
+echo ' sign2: '.$sign2.'<br>';
+echo ' sign3: '.$sign3.'<br>';
+
+
+
+?>
