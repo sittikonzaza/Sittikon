57aa5cadda0494076ca2f39271ef5d0580538da0
sittikonzaza <sittikon2345@gmail.com>
2023-11-21 11:05:43 +0700

    submit

--- pages/submit.php
+++ pages/submit.php
@@ -6,16 +6,43 @@ $email = $_POST['web'];
 $email = $_POST['role'];
 $email = $_POST['sign'];
 
+$server_name = 'localhost:8080';
+$username = 'root';
+$password = "";
+$dbname = "db_employee";
 
-echo ' email: '.$email.'<br>';
-echo ' password: '.$password.'<br>';
-echo ' city: '.$City.'<br>';
-echo ' web: '.$web.'<br>';
-echo ' role: '.$role.'<br>';
-echo ' sign: '.$sign.'<br>';
-echo ' sign1: '.$sign1.'<br>';
-echo ' sign2: '.$sign2.'<br>';
-echo ' sign3: '.$sign3.'<br>';
+$conn = new mysqli($severname, $username, $password, $dbname);
+if ($conn->connect_error) { 
+    die("". $conn->connect_error);
+
+}
+
+$sql ="INSERT INTO employee (username, password, city, webserver,role)
+       VALUES ($email,$password,$city,$web,$role)";
+
+$result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+    }  else{
+        echo"error".$sql."<br>".$conn->error;
+    }
+    $conn->close();
+
+
+
+
+
+// echo ' email: '.$email.'<br>';
+// echo ' password: '.$password.'<br>';
+// echo ' city: '.$City.'<br>';
+// echo ' web: '.$web.'<br>';
+// echo ' role: '.$role.'<br>';
+// echo ' sign: '.$sign.'<br>';
+// echo ' sign1: '.$sign1.'<br>';
+// echo ' sign2: '.$sign2.'<br>';
+// echo ' sign3: '.$sign3.'<br>';
 
 
 
